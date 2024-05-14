58f27d8808b5673a765303b2043bb314464b761c
Hasika <hasika20.2005@gmail.com>
2024-05-14 06:17:01 +0530

    DOne with login using the firebase authentication

--- hasika_cdh/src/components/InputField.jsx
+++ hasika_cdh/src/components/InputField.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react';
 
 const InputField = ({ handleChange, value, title, name }) => {
   return (
     <label className="sidebar-label-container">
-      <input type="radio" name={name} value={value} onChange={handleChange} />
+      <input
+        onChange={handleChange}
+        type="radio"
+        value={value} // Set the value prop as the input value
+        name={name}
+      />
       <span className="checkmark"></span>
       {title}
     </label>
--- hasika_cdh/src/components/Navbar.jsx
+++ hasika_cdh/src/components/Navbar.jsx
@@ -1,30 +1,45 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
-import { Link } from "react-router-dom";
+/* eslint-disable react/no-unknown-property */
+import React, { useContext, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import { FaBarsStaggered, FaXmark } from "react-icons/fa6";
+import { AuthContext } from "../context/AuthProvider";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { user, logOut } = useContext(AuthContext);
+  console.log(user)
+
+  
+  const handleLogout = () => {
+    logOut()
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // menu toggle btn
   const handleMenuToggler = () => {
     setIsMenuOpen(!isMenuOpen);
   };
-
-  const navIetms = [
+  const navItems = [
     { path: "/", title: "Start a search" },
     { path: "/my-job", title: "My Jobs" },
-    { path: "/salary", title: "Salary" },
-    { path: "/post-job", title: "Post a Job" },
+    { path: "/salary", title: "Salary estimate" },
+    { path: "/post-job", title: "Post A Job" },
   ];
   return (
-    <header className="max-w-screeen-2xl container mx-auto xl:px-24 px-4">
+    <header className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
       <nav className="flex justify-between items-center py-6">
-        <a href="/" className="flex items-center gap-2 text-2xl text-black">
+        <a href="/" className="flex items-center gap-2 text-2xl">
           <svg
-            width="29"
-            height="36"
-            viewBox="0 0 29 36"
-            fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            width="29"
+            height="30"
+            viewBox="0 0 29 30"
+            fill="none"
           >
             <circle
               cx="12.0143"
@@ -33,13 +48,14 @@ const Navbar = () => {
               fill="#3575E2"
               fillOpacity="0.4"
             />
-            <circle cx="16.9857" cy="20.4857" r="12.0143" fill="#3575E2" />
-          </svg>{" "}
-          <span>Career Development Hub</span>
+            <circle cx="16.9857" cy="17.4857" r="12.0143" fill="#3575E2" />
+          </svg>
+          <span>JobPortal</span>
         </a>
 
+        {/* nav items */}
         <ul className="hidden md:flex gap-12">
-          {navIetms.map(({ path, title }) => (
+          {navItems.map(({ path, title }) => (
             <li key={path} className="text-base text-primary">
               <NavLink
                 to={path}
@@ -51,41 +67,74 @@ const Navbar = () => {
           ))}
         </ul>
 
+        {/* sign up signout btn */}
         <div className="text-base text-primary font-medium space-x-5 hidden lg:block">
-          <Link to="/login" className="py-2 px-5 border rounded">
-            Sign In
-          </Link>
-          <Link
-            to="/sign-up"
-            className="py-2 px-5 border rounded bg-blue text-white"
-          >
-            Sign Up
-          </Link>
+          {user ? (
+            <>
+              <div className="flex gap-4 items-center">
+                <div class="flex -space-x-2 overflow-hidden">
+                  {
+                    user?.photoURL ? <> <img
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+                    src={user?.photoURL}
+                    alt=""
+                  /></> : <> <img
+                    className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  /></>
+                  }
+                 
+                </div>
+                <button onClick={handleLogout} className="py-2 px-5 border rounded hover:bg-blue hover:text-white">Log out</button>
+              </div>
+            </>
+          ) : (
+            <>
+              {" "}
+              <Link to="/login" className="py-2 px-5 border rounded">
+                Log in
+              </Link>
+              <Link
+                to="/sign-up"
+                className="bg-blue py-2 px-5 text-white rounded"
+              >
+                Sign up
+              </Link>
+            </>
+          )}
         </div>
 
+        {/* mobile menu */}
         <div className="md:hidden block">
           <button onClick={handleMenuToggler}>
             {isMenuOpen ? (
-              <FaXmark className="w-5 h-5 text-primary" />
+              <>
+                <FaXmark className="w-5 h-5 text-primary/75" />
+              </>
             ) : (
-              <FaBarsStaggered className="w-5 h-5 text-primary" />
+              <>
+                <FaBarsStaggered className="w-5 h-5 text-primary/75" />
+              </>
             )}
           </button>
         </div>
       </nav>
 
+      {/* mobile menu items */}
       <div
         className={`px-4 bg-black py-5 rounded-sm ${
           isMenuOpen ? "" : "hidden"
         }`}
       >
         <ul>
-          {navIetms.map(({ path, title }) => (
+          {navItems.map(({ path, title }) => (
             <li
               key={path}
               className="text-base text-white first:text-white py-1"
             >
               <NavLink
+                onClick={handleMenuToggler}
                 to={path}
                 className={({ isActive }) => (isActive ? "active" : "")}
               >
@@ -93,8 +142,9 @@ const Navbar = () => {
               </NavLink>
             </li>
           ))}
+
           <li className="text-white py-1">
-            <Link to="/login">Sign In</Link>
+            <Link to="login">Log in</Link>
           </li>
         </ul>
       </div>
